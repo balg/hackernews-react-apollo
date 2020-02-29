f22b9d14c6c3d8a4982b48391de182753a141ba1
balg <2700278+balg@users.noreply.github.com>
2020-02-29 14:36:26 +0100

    Basic components with mocked data

---
+++ src/components/Link/Link.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Link = props => {
+  const {
+    link: { description, url }
+  } = props;
+
+  return (
+    <div>
+      <div>
+        {description} ({url})
+      </div>
+    </div>
+  );
+};
+
+export default Link;
