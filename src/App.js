d8f58c55c8c1de5e29e131ddc7b781fb6d236a00
balg <2700278+balg@users.noreply.github.com>
2020-03-01 11:19:33 +0100

    Mutation component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import LinkList from './components/LinkList/LinkList';
+import CreateLink from './components/CreateLink/CreateLink';
 
 function App() {
   return (
-    <LinkList />
+    // <LinkList />
+    <CreateLink />
   );
 }
 
