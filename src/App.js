91ab12c84dde9e40906bc9fecc09572adbbff348
balg <2700278+balg@users.noreply.github.com>
2020-03-01 11:32:35 +0100

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import './App.css';
-import LinkList from './components/LinkList/LinkList';
-import CreateLink from './components/CreateLink/CreateLink';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header/Header";
+import LinkList from "./components/LinkList/LinkList";
+import CreateLink from "./components/CreateLink/CreateLink";
 
 function App() {
   return (
-    // <LinkList />
-    <CreateLink />
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={LinkList} />
+          <Route exact path="/create" component={CreateLink} />
+        </Switch>
+      </div>
+    </div>
   );
 }
 
--- src/components/CreateLink/CreateLink.js
+++ src/components/CreateLink/CreateLink.js
@@ -35,7 +35,11 @@ const CreateLink = props => {
           placeholder="The URL for the link"
         />
       </div>
-      <Mutation mutation={POST_MUTATION} variables={{ description, url }}>
+      <Mutation
+        mutation={POST_MUTATION}
+        variables={{ description, url }}
+        onCompleted={() => props.history.push('/')}
+      >
         {postMutation => <button onClick={postMutation}>Submit</button>}
       </Mutation>
     </div>
