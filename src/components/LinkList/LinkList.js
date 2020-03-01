22b5ccfd207c23f8c2835fa937d40241045dba26
balg <2700278+balg@users.noreply.github.com>
2020-03-01 11:05:54 +0100

    Using Query component and its render prop

--- src/components/LinkList/LinkList.js
+++ src/components/LinkList/LinkList.js
@@ -1,25 +1,39 @@
 import React from "react";
 import Link from "../Link/Link";
+import { gql } from "apollo-boost";
+import { Query } from "react-apollo";
+
+const FEED_QUERY = gql`
+  {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+      }
+    }
+  }
+`;
 
 const LinkList = props => {
-  const linksToRender = [
-    {
-      id: "1",
-      description: "Prisma turns your database into a GraphQL API 😎",
-      url: "https://www.prismagraphql.com"
-    },
-    {
-      id: "2",
-      description: "The best GraphQL client",
-      url: "https://www.apollographql.com/docs/react/"
-    }
-  ];
   return (
-    <div>
-      {linksToRender.map(link => (
-        <Link key={link.id} link={link} />
-      ))}
-    </div>
+    <Query query={FEED_QUERY}>
+      {({ loading, error, data }) => {
+        if (loading) return <p>Fetching...</p>;
+        if (error) return <p>Error</p>;
+
+        const linksToRender = data.feed.links;
+
+        return (
+          <div>
+            {linksToRender.map(link => (
+              <Link key={link.id} link={link} />
+            ))}
+          </div>
+        );
+      }}
+    </Query>
   );
 };
 
